8e3c4e3db44eb983549ad5c98b51d6c8dd153137
Zaqhry <96840072+Zaqhry@users.noreply.github.com>
2022-01-24 20:12:42 -0500

    Add files via upload

---
+++ Final FTP & TSL Results.sql
@@ -0,0 +1,58 @@
+
+
+
+
+--#### FINAL FTP & TSL RESULTS ####
+
+
+
+--FTP & TSL RESULTS
+
+SELECT * 
+FROM TOTALFTP ftp
+	INNER JOIN TOTALTSL tsl
+	ON ftp.session = tsl.session 
+
+--Most Profitable Confluences for each session FTP
+
+SELECT DISTINCT(Session),
+				Confluence,
+				ProfitLossFTP
+FROM AUDJPY 
+GROUP BY Session,
+		 Confluence,
+		 ProfitLossFTP
+	ORDER BY 1,
+			 3 DESC
+
+--Most Profitable Confluences for each session TSL
+
+SELECT DISTINCT(Session),
+				Confluence,
+				ProfitLossTSL
+FROM AUDJPY 
+GROUP BY Session,
+		 Confluence,
+		 ProfitLossTSL
+	ORDER BY 1,
+			 3 DESC
+
+--POSITIVE & NEGATIVE FTP For Each Confluence
+
+SELECT ProfitLossFTP,
+	   Confluence,
+	   COUNT(ProfitLossFTP) FTPOccurence
+FROM AUDJPY 
+	GROUP BY ProfitLossFTP,
+		 Confluence
+	ORDER BY FTPOccurence DESC;
+
+--POSITIVE & NEGATIVE TSL For Each Confluence
+
+SELECT ProfitLossTSL,
+	   Confluence,
+	   COUNT(ProfitLossTSL) TSLOccurence
+FROM AUDJPY 
+	GROUP BY ProfitLossTSL,
+			 Confluence
+	ORDER BY TSLOccurence DESC;
